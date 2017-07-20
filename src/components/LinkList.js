0967d4d5c979f34bec53734930785da8286759fe
Abraham Lapscher <ebirito@yahoo.com>
2017-07-19 23:45:48 -0400

    Added upvoting functionality and auto-refresh

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -4,43 +4,64 @@ import { graphql, gql } from 'react-apollo'
 
 class LinkList extends Component {
 
-  render() {
-    // 1
-    if (this.props.allLinksQuery && this.props.allLinksQuery.loading) {
-        return <div>Loading</div>
+    _updateCacheAfterVote = (store, createVote, linkId) => {
+        // 1
+        const data = store.readQuery({ query: ALL_LINKS_QUERY })
+
+        // 2
+        const votedLink = data.allLinks.find(link => link.id === linkId)
+        votedLink.votes = createVote.link.votes
+
+        // 3
+        store.writeQuery({ query: ALL_LINKS_QUERY, data })
     }
 
-    // 2
-    if (this.props.allLinksQuery && this.props.allLinksQuery.error) {
-        return <div>Error</div>
-    }
+    render() {
+        // 1
+        if (this.props.allLinksQuery && this.props.allLinksQuery.loading) {
+            return <div>Loading</div>
+        }
 
-    // 3
-    const linksToRender = this.props.allLinksQuery.allLinks
+        // 2
+        if (this.props.allLinksQuery && this.props.allLinksQuery.error) {
+            return <div>Error</div>
+        }
 
-    return (
-        <div>
-        {linksToRender.map(link => (
-            <Link key={link.id} link={link}/>
-        ))}
-        </div>
-    )
+        // 3
+        const linksToRender = this.props.allLinksQuery.allLinks
+
+        return (
+            <div>
+                {linksToRender.map((link, index) => (
+                    <Link key={link.id} updateStoreAfterVote={this._updateCacheAfterVote}  index={index} link={link}/>
+                ))}
+            </div>
+        )
     }
 
 }
 
 // 1
-const ALL_LINKS_QUERY = gql`
-  # 2
+export const ALL_LINKS_QUERY = gql`
   query AllLinksQuery {
     allLinks {
       id
       createdAt
       url
       description
+      postedBy {
+        id
+        name
+      }
+      votes {
+        id
+        user {
+          id
+        }
+      }
     }
   }
 `
 
 // 3
-export default graphql(ALL_LINKS_QUERY, { name: 'allLinksQuery' }) (LinkList)
+export default graphql(ALL_LINKS_QUERY, { name: 'allLinksQuery' })(LinkList)
